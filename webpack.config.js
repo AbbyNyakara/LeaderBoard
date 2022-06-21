3859836dff0e564750e0fcbe3b06ef3804ad032d
Abby Nyakara <abbynyakara@gmail.com>
2022-06-21 11:32:15 +0300

    correct linter errors

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
-const path = require("path");
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './docs/index.js',
@@ -7,22 +7,22 @@ module.exports = {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(js)$/,
-        use: "babel-loader",
+        use: 'babel-loader',
       },
-    ]
-  }, 
+    ],
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: './docs/template.html'
-    })
+    }),
   ],
   mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
-}
+};
