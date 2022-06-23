6f9c7d058316875cb5e75968d6048625a9241468
Abby Nyakara <abbynyakara@gmail.com>
2022-06-23 11:43:07 +0300

    correct linter errors

--- docs/index.js
+++ docs/index.js
@@ -1,17 +1,16 @@
 import './index.css';
 
 // Activate the refresh button to receive data from the API
-const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/lPlrZ7dDVaYlzQKx8q6V/scores'
+const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/lPlrZ7dDVaYlzQKx8q6V/scores';
 const refresh = document.querySelector('.refresh');
 const playerName = document.querySelector('.form .name');
 const playerScore = document.querySelector('.form .score');
 const scoreList = document.querySelector('.scores');
 const form = document.querySelector('.form');
 
-
 // Fetch scores from the API
-const fetchScores = async() => {
-  scoreList.innerHTML ='';
+const fetchScores = async () => {
+  scoreList.innerHTML = '';
   const response = await fetch(url);
   const dataFile = await response.json();
   const scoreData = dataFile.result;
@@ -23,28 +22,27 @@ const fetchScores = async() => {
   });
 };
 
-const addScores = async(userScores) => {
+const addScores = async (userScores) => {
   const res = await fetch(url, {
     method: 'POST',
     headers: {
-      'Content-Type' : 'application/json'
+      'Content-Type': 'application/json',
     },
-    body: JSON.stringify(userScores)
+    body: JSON.stringify(userScores),
   });
   return res;
-}
+};
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   const singleScore = {
     user: playerName.value,
     score: playerScore.value
-  }
-
+  };
   addScores(singleScore);
   form.reset();
-})
+});
 
 refresh.addEventListener('click', () => {
-  fetchScores()
+  fetchScores();
 });
